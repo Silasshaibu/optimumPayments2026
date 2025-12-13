078a5bbcf120dca9ab5f4e82556a473648c3b808
Silas Shaibu <92685385+Silasshaibu@users.noreply.github.com>
2025-12-13 12:47:40 +0100

    commmit

--- public_html/assets/js/topSellingProductsCategory.js
+++ public_html/assets/js/topSellingProductsCategory.js
@@ -1,6 +1,6 @@
-    //  Truncate Product Summary On Top Category
-        
-            //  Truncate Characters in product summary top categories← set your character limit here
+    //  Truncate Product Summary On Top Category        
+    //  Truncate Characters in product summary top categories← set your character limit here
+    
         document.addEventListener("DOMContentLoaded", () => {
             const maxChars = 55; // ← set your character limit here
             const summaries = document.querySelectorAll(".product-summary");
